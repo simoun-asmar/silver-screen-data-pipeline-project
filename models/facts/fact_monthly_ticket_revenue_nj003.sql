1f2810eb632d5691cbe0a3c1be44aeffccbce624
simoun-asmar <simon.asmar.sy@gmail.com>
2025-02-13 19:47:18 +0100

    Create fact_monthly_ticket_revenue_nj003.sql

---
+++ models/facts/fact_monthly_ticket_revenue_nj003.sql
@@ -0,0 +1,17 @@
+WITH monthly_revenue_nj003 AS (
+
+    SELECT 
+        DATE_TRUNC('month', timestamp) AS month,  -- Get the month from the timestamp
+        details AS movie_id,                     -- The movie we’re tracking revenue for
+        SUM(amount) AS tickets_sold,             -- Total number of tickets sold that month
+        SUM(total_value) AS revenue              -- Total revenue made from ticket sales
+
+    FROM {{ source('SILVER_SCREEN', 'NJ_003') }}  -- Pull data from the NJ_003 location
+
+    WHERE product_type = 'ticket'               -- Only include ticket sales, ignore other products
+    GROUP BY month, movie_id                    -- Group by movie and month to sum up sales
+    ORDER BY month                              -- Sort results by month so trends are clear
+
+)
+
+SELECT * FROM monthly_revenue_nj003
