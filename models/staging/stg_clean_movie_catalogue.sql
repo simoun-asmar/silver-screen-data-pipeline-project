12d068417c50dd092a3d43434b12e380f330e953
simoun-asmar <simon.asmar.sy@gmail.com>
2025-02-14 18:15:13 +0100

    Update stg_clean_movie_catalogue.sql

--- models/staging/stg_clean_movie_catalogue.sql
+++ models/staging/stg_clean_movie_catalogue.sql
@@ -5,12 +5,12 @@ WITH clean_movie_catalogue AS (
         movie_id,  -- Unique identifier for each movie
         movie_title,  -- Title of the movie
         release_date,  -- Date when the movie was released
-        COALESCE(genre, 'Unknown') AS genre,  -- Replaces NULL values in genre with 'Unknown'
+        {{ fill_nulls('genre') }} AS genre,  -- Use the fill_nulls macro to replace NULL values in column 'genre' with 'Unknown'
         country,  -- Country where the movie was produced
         studio,  -- Name of the studio that produced the movie
         budget,  -- Budget allocated for producing the movie
         director,  -- Name of the movie director
-        COALESCE(rating, 'Unknown') AS rating  -- Replaces NULL values in rating with 'Unknown'
+        {{ fill_nulls('rating') }} AS rating,  -- Use the fill_nulls macro to replace NULL values in column 'rating' with 'Unknown'
 
     FROM {{ source('SILVER_SCREEN', 'MOVIE_CATALOGUE') }}  -- Reference to the source table
 
